f4a6044d57aa86a6ccfe2e56040e381e557cb6d1
KD-67 <kevin.a.dominey@gmail.com>
2023-04-16 12:45:00 +0300

    working one round game

--- javascript.js
+++ javascript.js
@@ -1,7 +1,34 @@
-function getComputerChoice(){
-    return Math.floor(Math.random() * 3);
+// 0-ROCK; 1-PAPER; 2-SCISSORS
+
+let playerChoice = prompt("Choose rock paper or scissors");
+let computerChoice = Math.floor(Math.random() * 3);
+
+playerChoice = playerChoice.toUpperCase();
+
+if (playerChoice === "ROCK"){
+    playerChoice = 0;
+} else if (playerChoice === "PAPER"){
+    playerChoice = 1;
+} else if (playerChoice === "SCISSORS"){
+    playerChoice = 2;
+} else {
+    console.log("Please enter either 'rock', paper', or 'scissors'.")
 }
 
-function oneGame (playerSelection, computerSelaction){
-    
+
+
+if (computerChoice === playerChoice) {
+    console.log("It's a tie!")
+} else if (computerChoice === 0 && playerChoice === 1){
+    console.log("You win! Paper beats rock!");
+} else if (computerChoice === 0 && playerChoice === 2){
+    console.log("You lose! Rock beats scissors!");
+} else if (computerChoice === 1 && playerChoice === 0){
+    console.log("You lose! Paper beats rock!");
+} else if (computerChoice === 1 && playerChoice === 2){
+    console.log("You win! Scissors beats paper!");
+} else if (computerChoice === 2 && playerChoice === 0){
+    console.log("You win! Rock beats scissors!");
+} else if (computerChoice === 2 && playerChoice === 1){
+    console.log("You lose! Scissors beats paper!");
 }
